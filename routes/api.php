d3b571351813b55dde3c99bbbc0fd9212a9de8af
Saladin <saladinoctianobonanza@gmail.com>
2026-04-25 21:06:23 +0700

    feat(add): CRUD Article

--- routes/api.php
+++ routes/api.php
@@ -3,17 +3,18 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\BeasiswaController;
+use App\Http\Controllers\Api\ArtikelController;
 
-
+// Beasiswa List
 // GET
 Route::get('/beasiswa', [BeasiswaController::class, 'index']);
 Route::get('/beasiswa/{id}', [BeasiswaController::class, 'show']);
-
 // POST
 Route::post('/beasiswa', [BeasiswaController::class, 'store']);
-
 // DELETE
 Route::delete('/beasiswa/{id}', [BeasiswaController::class, 'destroy']);
-
 // PUT
 Route::put('/beasiswa/{id}', [BeasiswaController::class, 'update']);
+
+// Artikel
+Route::apiResource('artikel', ArtikelController::class);
